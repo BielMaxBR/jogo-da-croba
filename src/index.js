ce60f82f0d1888b9ae8a910d68ddf23586931169
Gabriel Maximo <gbielmaximo@gmail.com>
2020-04-28 23:54:26 +0000

    loop infinito

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-var canvas = document.getElementById("stage")
-console.log(canvas)
+var canvas, ctx, WIDTH, HEIGHT, bpm;
+function init() {
+    canvas = document.createElement("canvas")
+    canvas.width = 600
+    canvas.height = 600
+    bpm = 120
+
+    document.body.appendChild(canvas)
+    ctx = canvas.getContext("2d");
+
+    run()
+}
+
+function update() {
+    console.log('pum')
+ 
+}
+ 
+function run() {
+    update()
+    draw()
+    setTimeout(run, 60/bpm*1000)
+}
+
+function draw() {}
+
+init()
