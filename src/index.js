586d879f03e6648a5bc912b2ee13a3dea3bc318a
Gabriel Maximo <gbielmaximo@gmail.com>
2020-04-29 20:05:42 +0000

    a cobra existe

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ function init() {
     
     bpm = 120
 
+    newGame()
     run()
 }
 function resizeWindow() {
@@ -20,6 +21,23 @@ function resizeWindow() {
     canvas.height = HEIGHT
 
     tileSize = Math.max(Math.floor(WIDTH / 60), Math.floor(HEIGHT / 60))
+}
+
+function newGame() {
+    snake = new Snake()
+}
+
+function Snake() {
+    this.body = [[10,10],[10,11],[10,12]]
+    this.color = '#000'
+
+    this.draw = function() {
+        ctx.fillStyle = this.color
+
+        for (var i = 0; i < this.body.length; i++) {
+            ctx.fillRect(this.body[i][0] * tileSize, this.body[i][1] * tileSize, tileSize, tileSize)
+        }
+    }
 }
 
 function update() {
@@ -33,6 +51,10 @@ function run() {
     setTimeout(run, 60/bpm*1000)
 }
 
-function draw() {}
+function draw() {
+    ctx.clearRect(0 ,0 ,WIDTH, HEIGHT)
 
+    snake.draw()
+}
+ 
 init()
