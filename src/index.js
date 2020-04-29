a712486b85784ef0cc76d4a3a34fb4fac1989f46
Gabriel Maximo <gbielmaximo@gmail.com>
2020-04-29 00:23:55 +0000

    pausa pra noite

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-var canvas, ctx, WIDTH, HEIGHT, bpm;
+var canvas, ctx, WIDTH, HEIGHT, bpm, tileSize;
+
+window.addEventListener("resize", resizeWindow)
+
 function init() {
     canvas = document.createElement("canvas")
-    canvas.width = 600
-    canvas.height = 600
-    bpm = 120
-
+    resizeWindow()
     document.body.appendChild(canvas)
     ctx = canvas.getContext("2d");
+    
+    bpm = 120
 
     run()
+}
+function resizeWindow() {
+    WIDTH = window.innerWidth
+    HEIGHT = window.innerHeight
+
+    canvas.width = WIDTH
+    canvas.height = HEIGHT
+
+    tileSize = Math.max(Math.floor(WIDTH / 60), Math.floor(HEIGHT / 60))
 }
 
 function update() {
